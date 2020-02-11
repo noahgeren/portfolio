46fbedfe2f3745ac37116ae6af83b374d6f33110
Noah <noah.s.geren@gmail.com>
2020-02-10 22:55:42 -0600

    Initial commit.

---
+++ src/main/java/tech/noahgeren/portfolio/PorfolioApplication.java
@@ -0,0 +1,13 @@
+package tech.noahgeren.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/noahgeren/portfolio/PorfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noahgeren.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
